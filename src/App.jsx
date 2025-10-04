7a67d50d0d94478e20b00f9884227beb0c0a5c82
Abubakr kazi <kaziabubakr87@gmail.com>
2025-10-04 20:56:18 +0600

    Fifth commit on useEffect  with async,await

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,28 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
+
+
 
 const App = () => {
-  const [list, setList] = useState([]);
-  const [item, setItem] = useState("");
+  let [data,setData]=useState();
+  
+useEffect(()=>{
+( async()=>{
+    let responce=await fetch("https://dummyjson.com/products/2")
+    let json=await responce.json()
+    setData(json)
+})()
 
-  const AddTolist = () => {
-    if(item.trim() !== ""){   // ফাঁকা item যেন না যায়
-      setList([...list, item]); 
-      setItem(""); // add করার পর ইনপুট খালি
-    }
-  };
 
-  const Remove = (index) => {
-    list.splice(index, 1); // শুধু ১ দিতে হবে
-    setList([...list]); 
-  };
+},[])
+
+
+
 
   return (
     <div>
-      <table>
-        <tbody>
-          {
-            list.length !== 0 ? (
-              list.map((element,index) => {
-                return (
-                  <tr key={index}>
-                    <td>{index+1}. {element}</td>
-                    <td>
-                      <button onClick={()=>Remove(index)}>
-                        Remove
-                      </button>
-                    </td>
-                  </tr>
-                )
-              })
-            ) : (
-              <tr>
-                <td>No items</td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
-
-      <input 
-        value={item} 
-        onChange={(e) => setItem(e.target.value)} 
-        placeholder="Item" 
-      />
-      <button onClick={AddTolist}>Add</button>
+      {
+        JSON.stringify(data)
+      }
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>,
+  
 )
