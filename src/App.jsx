17cdaa6db7bb1042678bddd503ae50ca1c1e52e9
Abubakr kazi <kaziabubakr87@gmail.com>
2025-10-04 19:03:16 +0600

    Fourth commit on usestate ToDo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
-import { useRef } from "react";
-//useRef() Caching expensive computations
+import React, { useState } from 'react';
+
 const App = () => {
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState("");
 
-  let APIData = useRef(null);
-  let myPtag = useRef();
+  const AddTolist = () => {
+    if(item.trim() !== ""){   // ফাঁকা item যেন না যায়
+      setList([...list, item]); 
+      setItem(""); // add করার পর ইনপুট খালি
+    }
+  };
 
-  const fetchData = async () => {
-    const response = await fetch("https://dummyjson.com/products");
-    APIData.current = await response.json();
-  }
-
-  const showData = () => {
-    myPtag.current.innerText = JSON.stringify(APIData.current);
-  }
+  const Remove = (index) => {
+    list.splice(index, 1); // শুধু ১ দিতে হবে
+    setList([...list]); 
+  };
 
   return (
     <div>
-      <p ref={myPtag}></p>
-      <button onClick={fetchData}>Call API</button>
-      <button onClick={showData}>Show Data</button>
+      <table>
+        <tbody>
+          {
+            list.length !== 0 ? (
+              list.map((element,index) => {
+                return (
+                  <tr key={index}>
+                    <td>{index+1}. {element}</td>
+                    <td>
+                      <button onClick={()=>Remove(index)}>
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            ) : (
+              <tr>
+                <td>No items</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+
+      <input 
+        value={item} 
+        onChange={(e) => setItem(e.target.value)} 
+        placeholder="Item" 
+      />
+      <button onClick={AddTolist}>Add</button>
     </div>
   );
 };
